91f4eba26610c081ba2a210a82e1b0a81eb07d0c
Виктор <StatingWaif68@gmail.com>
2024-07-12 08:02:07 +0300

    af

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import "./styles/reset.css";
-import "./styles/styles.css";
+// import "./styles/styles.css";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 export const App = () => {
