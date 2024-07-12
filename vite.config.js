3709a4c7032c06450b8f8dca98ec72c056ed272d
Виктор <StatingWaif68@gmail.com>
2024-07-12 05:32:01 +0300

    d

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ViteMinifyPlugin from "vite-plugin-minify";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
