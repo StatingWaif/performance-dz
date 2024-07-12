865eb31fe7b6b55c464f2e86b87dc6df3c0f8aa5
Виктор <StatingWaif68@gmail.com>
2024-07-12 06:36:36 +0300

    prikol

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
+import PreloadWebpackPlugin from "preload-webpack-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/performance-dz/",
   plugins: [react(), ViteMinifyPlugin()],
+  configureWebpack: (config) => {
+    config.plugins.push(
+      new PreloadWebpackPlugin({
+        rel: "preload",
+        include: "allAssets",
+        fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+      })
+    );
+    return config;
+  },
 });
