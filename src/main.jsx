58701ca23fda87f76a597918d29613f859b5b3e2
Виктор <StatingWaif68@gmail.com>
2024-07-12 12:34:54 +0300

    message with words

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { Header } from "./components/Header.jsx";
 import { FavoriteDevicesSection } from "./components/FavoriteDevicesSection.jsx";
-
 ReactDOM.createRoot(document.getElementById("header")).render(<Header />);
 
 ReactDOM.createRoot(document.getElementById("favoriteRoot")).render(
