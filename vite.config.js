22ffc1b5b4f85b60ac2dd7348e3c9edfbf68c194
Виктор <StatingWaif68@gmail.com>
2024-07-12 05:29:56 +0300

    fdf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import ViteMinifyPlugin from "vite-plugin-minify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/performance-dz/",
-  plugins: [react()],
+  plugins: [react(), ViteMinifyPlugin()],
 });
