1d687c0d08a3a4154b17d4a5acb87e02957b856f
Виктор <StatingWaif68@gmail.com>
2024-07-12 15:28:26 +0300

    i did all i could

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { Header } from "./components/components/Header.jsx";
 import { FavoriteDevicesSection } from "./components/components/FavoriteDevicesSection.jsx";
+import "./styles/styles.css";
 ReactDOM.createRoot(document.getElementById("header")).render(<Header />);
 
 ReactDOM.createRoot(document.getElementById("favoriteRoot")).render(
